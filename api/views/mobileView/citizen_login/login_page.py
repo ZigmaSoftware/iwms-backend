910487876f9e67ee8d33714bbb68f9d31396ac28
ZigmaSoftware <creative@zigma.in>
2025-11-20 11:54:21 +0530

    login module backend connectivity

---
+++ api/views/mobileView/citizen_login/login_page.py
@@ -0,0 +1,21 @@
+from django.urls import reverse
+from django.views.generic import TemplateView
+from api.apps.customercreation import CustomerCreation
+from api.apps.userType import UserType
+
+
+class CitizenLoginPage(TemplateView):
+    template_name = "mobile/citizen_login.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_types"] = list(
+            UserType.objects.filter(is_active=True, is_delete=False).order_by("name")
+        )
+        context["customers"] = list(
+            CustomerCreation.objects.filter(is_active=True, is_deleted=False)
+            .order_by("customer_name")
+            .values("customer_name", "contact_no")[:80]
+        )
+        context["login_path"] = reverse("customer-login")
+        return context
