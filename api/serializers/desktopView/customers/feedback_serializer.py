8cbec4d4db5eadca56987a86cf5374cb9306b52d
ZigmaSoftware <creative@zigma.in>
2025-11-26 10:20:12 +0530

    error in continent,country,feedback

--- api/serializers/desktopView/customers/feedback_serializer.py
+++ api/serializers/desktopView/customers/feedback_serializer.py
@@ -2,8 +2,10 @@ from rest_framework import serializers
 from api.apps.feedback import FeedBack
 
 class FeedBackSerializer(serializers.ModelSerializer):
+    customer_id = serializers.CharField(source="customer.unique_id", read_only=True)
+    customer_name = serializers.CharField(source="customer.customer_name", read_only=True)
     ward_name = serializers.CharField(source="customer.ward.ward_name", read_only=True)
-    zone_name = serializers.CharField(source="customer.zone.name", read_only=True, default=None)
+    zone_name = serializers.CharField(source="customer.zone.name", read_only=True)
     city_name = serializers.CharField(source="customer.city.name", read_only=True)
 
     class Meta:
