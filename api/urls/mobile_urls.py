7e4d71cc62636ca9c833233b12e44fd74332900d
ZigmaSoftware <creative@zigma.in>
2025-11-26 22:44:30 +0530

    dhivya after login role based route

--- api/urls/mobile_urls.py
+++ api/urls/mobile_urls.py
@@ -4,12 +4,13 @@ from rest_framework.routers import DefaultRouter
 from ..views.mobileView.citizen_login.login_viewset import CitizenLoginViewSet  # ViewSet version
 from ..views.mobileView.grievance.main_category_viewset import MainCategoryViewSet
 from ..views.mobileView.grievance.sub_category_viewset import SubCategoryViewSet
+from ..views.mobileView.citizen_login.new_login_views import LoginViewSet
 # from ..views.mobileView.grievance.sub_category_viewset import 
 
 router = DefaultRouter()
 router.register("customer/login", CitizenLoginViewSet, basename="customer-login")
 router.register("main-category", MainCategoryViewSet, basename="main-category")
 router.register("subcategory", SubCategoryViewSet, basename="subcategory")
-
+router.register("login", LoginViewSet, basename="mobile-login")
 urlpatterns = router.urls
 
