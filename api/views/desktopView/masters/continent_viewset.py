77e094c649a6974cb192e733ee42b8f69db31fba
SathyavardhanZigma <sathyavardhan@zigmaglobal.in>
2025-12-02 15:27:15 +0530

    fixed continent and country

--- api/views/desktopView/masters/continent_viewset.py
+++ api/views/desktopView/masters/continent_viewset.py
@@ -2,7 +2,10 @@ from rest_framework import viewsets
 from api.apps.continent import Continent
 from api.serializers.desktopView.masters.continent_serializer import ContinentSerializer
 
-
 class ContinentViewSet(viewsets.ModelViewSet):
     queryset = Continent.objects.filter(is_delete=False)
     serializer_class = ContinentSerializer
+    lookup_field = "unique_id"
+
+    def perform_destroy(self, instance):
+        instance.delete()
--- api/views/desktopView/masters/country_viewset.py
+++ api/views/desktopView/masters/country_viewset.py
@@ -3,11 +3,9 @@ from api.apps.country import Country
 from api.serializers.desktopView.masters.country_serializer import CountrySerializer
 
 class CountryViewSet(viewsets.ModelViewSet):
-    queryset = Country.objects.filter(is_delete=False).order_by("name")
+    queryset = Country.objects.filter(is_delete=False)
     serializer_class = CountrySerializer
+    lookup_field = "unique_id"
 
     def perform_destroy(self, instance):
-        """
-        Override DELETE → soft delete only
-        """
         instance.delete()
