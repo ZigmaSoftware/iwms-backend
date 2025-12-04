d35a8f2e969724ae5573f8fe40ecfecc2b9cee3b
SathyavardhanZigma <sathyavardhan@zigmaglobal.in>
2025-12-04 15:16:39 +0530

    property and subproperty fixed

--- api/serializers/desktopView/assets/property_serializer.py
+++ api/serializers/desktopView/assets/property_serializer.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
 from api.apps.property import Property
+from api.validators.unique_name_validator import unique_name_validator
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = '__all__'
+        fields = "__all__"
+        read_only_fields = ["unique_id"]
+        validators = []
+
+    def validate(self, attrs):
+        return unique_name_validator(
+            Model=Property,
+            name_field="property_name",
+        )(self, attrs)
