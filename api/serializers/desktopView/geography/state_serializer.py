171979060a75904cfd784736f54af67571d9c130
ZigmaSoftware <creative@zigma.in>
2025-12-22 18:26:13 +0530

    modify

---
+++ api/serializers/desktopView/geography/state_serializer.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from api.apps.state import State
+
+class StateSerializer(serializers.ModelSerializer):
+    country_name = serializers.CharField(source='country.name', read_only=True)
+
+    class Meta:
+        model = State
+        fields = '__all__'
