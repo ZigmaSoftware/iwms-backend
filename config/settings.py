19d717d2a321c2123cc705f7d10306caa3db5262
lakshana-59 <lakshanadharshini5526@gmail.com>
2026-03-18 18:10:26 +0530

    db config changed

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'iwmsdb',
         'USER': 'root',
-        'PASSWORD': 'Lakshana@123',
+        'PASSWORD': 'admin@123',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
